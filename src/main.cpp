e3da7e6f1fa6b6a796338d627d99720d2b71cc0d
ArmanKh9 <khalighi.ar9@gmail.com>
2018-01-06 12:53:37 -0500

    1

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,7 @@ int main()
   // TODO: Initialize the pid variable.
   double Kp, Ki, Kd;
   pid.init(Kp, Ki, Kd);
+  pid.p_error;
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -59,7 +60,6 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          pid.p_error = 0.0;
           pid.UpdateError(cte);
           steer_value = -Kp*pid.p_error - Kd*pid.d_error - Ki*pid.i_error;
 
